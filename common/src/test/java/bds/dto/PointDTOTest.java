23422f052ce1c1cdbe6f9b454f45f75dd0f25492
den3011den <den3011den@gmail.com>
2018-05-20 00:39:40 +0700

    добавление decodeDto() в PointDTOTest.java (common/test/java/bds/dto) и тестирование

--- common/src/test/java/bds/dto/PointDTOTest.java
+++ common/src/test/java/bds/dto/PointDTOTest.java
@@ -1,8 +1,10 @@
 package bds.dto;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -10,13 +12,26 @@ import static org.junit.Assert.assertTrue;
  */
 public class PointDTOTest {
 
+    private String expected = "{\"lat\":56.0,\"lon\":74.0,\"autoId\":\"o567gfd\",\"time\":1526751215894}";
+    private String autoID = "o567gfd";
+
     @Test
     public void toJson() throws Exception {
         PointDTO point = new PointDTO();
         point.setLat(56);
         point.setLon(74);
         point.setAutoId("o567gfd");
+        point.setTime(System.currentTimeMillis());
         assertTrue(point.toJson().contains("\"lat\":56"));
+        assertTrue(point.toJson().contains("\"time\":"));
         System.out.println(point.toJson());
+    }
+
+    @Test
+    public void decodeDto() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        PointDTO dto = mapper.readValue(expected, PointDTO.class);
+        assertEquals(autoID, dto.getAutoId());
+        assertEquals(1526751215894L, dto.getTime());
     }
 }
