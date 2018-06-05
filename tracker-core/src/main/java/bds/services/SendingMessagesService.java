5aa0c49500a24673c39579c3f72a76ac7acdf2ed
den3011den <den3011den@gmail.com>
2018-06-06 00:01:26 +0700

    Рабочий вавиант с логированием

--- tracker-core/src/main/java/bds/services/SendingMessagesService.java
+++ tracker-core/src/main/java/bds/services/SendingMessagesService.java
@@ -12,23 +12,35 @@ import bds.GpsContext;
 import bds.dto.PointDTO;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static bds.GpsContext.savingMessagesService;
 
 @Service
 public class SendingMessagesService {
 
-        @Scheduled(cron = "${gpstracker.sendSchedule.cron.prop}")
-        private void tick() throws IOException, InterruptedException {
+    private static final Logger log = LoggerFactory.getLogger(SendingMessagesService.class);
 
-            System.out.println("SendingMessagesService.tick " );
-
-            String jsonPointDTO;
-            jsonPointDTO = GpsContext.savingMessagesService.takePointDTOFromQueue();
-
-        }
-
+    private String getPointFromQueue() throws IOException, InterruptedException{
+        return GpsContext.savingMessagesService.takePointDTOFromQueue();
     }
+
+    private void sendPointToServer(String pointToSendInJson) throws IOException, InterruptedException{
+        log.info("SendingMessagesService: " + pointToSendInJson);
+    }
+
+
+    @Scheduled(cron = "${gpstracker.sendSchedule.cron.prop}")
+     private void tick() throws IOException, InterruptedException {
+
+        System.out.println("SendingMessagesService.tick " );
+
+        String jsonPointDTO;
+        jsonPointDTO = getPointFromQueue();
+        sendPointToServer(jsonPointDTO);
+
+     }
+}
 
