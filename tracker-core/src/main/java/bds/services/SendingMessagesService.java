0b0f9a4086bf986c6abfbab6cf336e703e4766c4
den3011den <den3011den@gmail.com>
2018-06-09 21:06:59 +0700

    Переделка отправки данных в tracker-core

--- tracker-core/src/main/java/bds/services/SendingMessagesService.java
+++ tracker-core/src/main/java/bds/services/SendingMessagesService.java
@@ -11,12 +11,16 @@ import bds.GpsContext;
 import bds.dto.PointDTO;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -41,13 +45,31 @@ public class SendingMessagesService {
         LOG.info("SendingMessagesService: " + pointToSendInJson);
 
         RestTemplate restTemplate = new RestTemplate();
+
+        String url = "http://localhost:8080/coords";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(pointToSendInJson, headers);
         try {
-            PointDTO hhh =  new PointDTO(1, 1,"eee", 2222L);
-            restTemplate.postForObject("http://localhost:8080/coords", hhh, PointDTO.class);
+            String answer = restTemplate.postForObject(url, entity, String.class);
+            System.out.println(answer);
+        }
+        catch (ResourceAccessException ee) {
+            LOG.info("SendingMessagesService: cannot send. Connection refused for: http://localhost:8080/coords");
         }
         catch (HttpClientErrorException ee) {
-            LOG.info("SendingMessagesService: cannot send. Did not find server or url: http://localhost:8080/coords");
+            LOG.info("SendingMessagesService: cannot send. Error 404 for: http://localhost:8080/coords");
         }
+
+//
+//        try {
+//            PointDTO hhh =  new PointDTO(1, 1,"eee", 2222L);
+//            restTemplate.postForObject("http://localhost:8080/coords", hhh, PointDTO.class);
+//        }
+//        catch (ResourceAccessException ee) {
+//            LOG.info("SendingMessagesService: cannot send. Did not find server or url: http://localhost:8080/coords");
+//        }
 
     }
 
