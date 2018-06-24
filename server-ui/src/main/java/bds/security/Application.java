bcd833d7261181b8ab6c7b95a45571c72853e4e6
den3011den <den3011den@gmail.com>
2018-06-25 00:15:50 +0700

    Исправление имени пакета в диррективе @ComponentScan в Application.java

--- server-ui/src/main/java/bds/security/Application.java
+++ server-ui/src/main/java/bds/security/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("jd.config")
+@ComponentScan("bds.config")
 public class Application {
 
     public static void main(String[] args) throws Throwable {
