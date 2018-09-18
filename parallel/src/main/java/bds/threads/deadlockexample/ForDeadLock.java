428114fbcb9e40545fc28a6c63b142f5c4a17833
den3011den <den3011den@gmail.com>
2018-09-19 06:25:36 +0700

    "\parallel\src\main\java\bds\threads\deadlockexample" - пример взаимной блокировки типа deadlock - чуть более наглядный вывод в консоль

--- parallel/src/main/java/bds/threads/deadlockexample/ForDeadLock.java
+++ parallel/src/main/java/bds/threads/deadlockexample/ForDeadLock.java
@@ -40,18 +40,24 @@ public class ForDeadLock implements Runnable {
     public void run() {
         if (flag < 5) {
             synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + ": заблокировал lock1");
                 synchronized (lock2) {
+                    System.out.println(Thread.currentThread().getName() + ": заблокировал lock1 и lock2");
                     testMetod1();
                     testMetod2();
                 }
             }
+            System.out.println(Thread.currentThread().getName() + ": освободил lock1 и lock2");
          }
         else {
             synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + ": заблокировал lock2");
                 synchronized (lock1) {
+                    System.out.println(Thread.currentThread().getName() + ": заблокировал lock2 и lock1");
                     testMetod2();
                     testMetod1();
                 }
+                System.out.println(Thread.currentThread().getName() + ": освободил lock2 и lock1");
             }
         }
 
